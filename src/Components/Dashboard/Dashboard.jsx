e48704d45144c0f2be04f15ec776bb99b5338477
Tarun Jeevan <tjeevan200@gmail.com>
2023-10-19 21:17:22 -0400

    Just created component skeletons

---
+++ src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,22 @@
+
+
+export default function Dashboard() {
+
+    return (
+        <>
+            <SurveyButton>
+                
+            </SurveyButton>
+        </>
+    )
+}
+
+// Survey button within the dashboard that creates new surveys onclick
+function SurveyButton() {
+
+    return (
+        <>
+
+        </>
+    )
+}
