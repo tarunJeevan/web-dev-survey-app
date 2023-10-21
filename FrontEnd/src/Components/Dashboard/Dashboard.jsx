9cc766664f474021dad2257618147684dfe116a1
Tarun Jeevan <tjeevan200@gmail.com>
2023-10-21 14:08:28 -0400

    Dashboard mostly complete
    Styled the dashboard (would appreciate some comments on it). SurveyCard component will be used to fetch and render analytics and other details from database on whatever survey user clicks on in the dashboard.
    
    Added routes in index.js for the dashboard and a survey creator page (I want to try using SurveyJS to create a form builder).
    
    Changed Login.jsx so it redirects to /dashboard instead of /profile (just wanted to note that).

--- FrontEnd/src/Components/Dashboard/Dashboard.jsx
+++ FrontEnd/src/Components/Dashboard/Dashboard.jsx
@@ -1,64 +1,74 @@
 import './Dashboard.css'
+import SurveyCard from './SurveyCard'
+import { useNavigate } from 'react-router-dom'
 
-export default function Dashboard() {
+export function Dashboard() {
 
     const toggleArrow = (index) => {
         let arrow = document.getElementById(index)
         arrow.innerHTML = (arrow.innerHTML === "⊳") ? "&#x22BF;" : "&#x22B3;"
     }
 
+    // This variable should contain an array of surveys created by the user to be summarized here
     let surveys = [
         {
+            id: crypto.randomUUID(),
             name: "Example survey",
             description: "Description"
         },
         {
+            id: crypto.randomUUID(),
             name: "Example survey",
             description: "Description"
         },
         {
+            id: crypto.randomUUID(),
             name: "Example survey",
             description: "Description"
         },
         {
+            id: crypto.randomUUID(),
             name: "Example survey",
             description: "Description"
-        }] // This variable should contain an array of surveys created by the user to be summarized here
-        
+        }]
+
     return (
         <>
             <div className="dashboard-header">
-                <h2>Dashboard</h2>
+                <h1>Dashboard</h1>
                 <NewSurveyButton>
 
                 </NewSurveyButton>
             </div>
 
-            {surveys.map((survey, index) => {
+            <div className='survey-container'>
+                {surveys.map((survey, index) => {
 
-                return (
-                    <details className="survey-summary" key={index}>
-                        <summary onClick={() => toggleArrow(index)}>
-                            {survey.name}
-                            <span className="summary-arrow" id={index} >
-                                &#x22B3;
-                            </span>
-                        </summary>
-                        {survey.description}
-                    </details>
-                )
-            })}
+                    return (
+                        <details className="survey-details" key={survey.id}>
+                            <summary onClick={() => toggleArrow(index)}>
+                                {survey.name}
+                                <span className="summary-arrow" id={index} >
+                                    &#x22B3;
+                                </span>
+                            </summary>
+                            <SurveyCard surveyID={survey.id} />
+                        </details>
+                    )
+                })}
+            </div>
         </>
     )
 }
 
 // Survey button within the dashboard that creates new surveys onclick
 function NewSurveyButton() {
+    const navigate = useNavigate()
 
     return (
         <>
-            <button className="add-btn" data-descr="Create a new survey...">
-                +  {/* Change to text or image if necessary */}
+            <button className="add-btn" data-descr="Create a new survey..." onClick={() => navigate("/creator")}>
+                Add  {/* Change to text or image if necessary */}
             </button>
         </>
     )
--- FrontEnd/src/Components/Dashboard/SurveyCard.jsx
+++ FrontEnd/src/Components/Dashboard/SurveyCard.jsx
@@ -1,12 +1,13 @@
-
+import './SurveyCard.css'
 
 export default function SurveyCard({ surveyID }) {
 
     // Should return a div containing analytics for a specific survey
     return (
         <>
-            <div>
+            <div className='card-container'>
                 Survey details here!
+                {/* Get analytics using SurveyJS API */}
             </div>
         </>
     )
