c60482308bdc69a54adb282ebb74634625f32395
Tarun Jeevan <tjeevan200@gmail.com>
2023-12-01 21:33:53 -0500

    Can add questions now. Most need additional options though.

--- FrontEnd/src/Components/SurveyBuilder/SurveyBuilder.jsx
+++ FrontEnd/src/Components/SurveyBuilder/SurveyBuilder.jsx
@@ -1,76 +1,116 @@
 import 'survey-core/defaultV2.min.css'
 import { Model } from 'survey-core'
 import { Survey } from 'survey-react-ui'
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import "./SurveyBuilder.css"
 
 const allQuestionTypes = [
     { name: 'Textbox', type: 'text' },
-    { name: 'Multiple Textboxes', type: 'multipletext' },
+    { name: 'Multiple Textboxes', type: 'multipletext' }, // TODO: Needs additional options
     { name: 'Comment', type: 'comment' },
-    { name: 'Radio Button Group', type: 'radiogroup' },
-    { name: 'Rating Scale', type: 'rating' },
-    { name: 'Checkboxes', type: 'checkbox' },
-    { name: 'Dropdown', type: 'dropdown' },
-    { name: 'Multi-Select Dropdown', type: 'tagbox' },
+    { name: 'Radio Button Group', type: 'radiogroup' }, // TODO: Needs additional options
+    { name: 'Rating Scale', type: 'rating' }, // TODO: Needs additional options
+    { name: 'Checkboxes', type: 'checkbox' }, // TODO: Needs additional options
+    { name: 'Dropdown', type: 'dropdown' }, // TODO: Needs additional options
+    { name: 'Multi-Select Dropdown', type: 'tagbox' }, // TODO: Needs additional options
     { name: 'Yes/No', type: 'boolean' },
-    { name: 'Ranking', type: 'ranking' },
-    { name: 'Image Picker', type: 'imagepicker' },
-    { name: 'Single-Select Matrix', type: 'matrix' }
+    { name: 'Ranking', type: 'ranking' }, // TODO: Needs additional options
+    { name: 'Image Picker', type: 'imagepicker' }, // TODO: Needs additional options
+    { name: 'Single-Select Matrix', type: 'matrix' } // TODO: Needs additional options
 ]
 
 export function SurveyBuilder() {
-    const surveyModel = new Model()
-
-    return (
-        <div id="container">
-            <aside id="question-types">
-                <h2>Question Types</h2>
-                {allQuestionTypes.map((value, index) => {
-
-                    return <QuestionTypeButton key={index} title={value.name} typeValue={value.type} surveyModel={surveyModel} />
-                })}
-            </aside>
-
-            <main id="question-container">
-                <Survey model={surveyModel} />
-            </main>
-
-            <aside id="question-details">
-                <h2>Details</h2>
-            </aside>
-        </div>
-    )
-}
-
-function QuestionTypeButton({ title, typeValue, surveyModel }) {
+    const [questionType, setQuestionType] = useState("")
+    const [surveyJson, setSurveyJson] = useState({
+        pages: [{
+            name: "Page1",
+            elements: [{
+                type: "text",
+                name: "Default",
+                title: "Default Survey Question"
+            }]
+        }]
+    })
     const dialogRef = useRef()
     const inputRef = useRef()
+
+    const survey = new Model(surveyJson)
 
     const addNewQuestion = e => {
         e.preventDefault()
 
-        // TODO: Get form data and create new question to add to surveyQuestions
+        const title = inputRef.current.value
+
+        setSurveyJson((prevJson) => {
+            return {
+                pages: [{
+                    name: "Page1",
+                    elements: [
+                        ...prevJson.pages[0].elements,
+                        {
+                            type: questionType,
+                            name: title.replace(/\s/g, ''),
+                            title: title
+                        }
+                    ]
+                }]
+            }
+        })
 
         inputRef.current.value = ''
         dialogRef.current.close()
     }
 
+    const openModal = type => {
+        inputRef.current.value = ''
+        setQuestionType(type)
+        dialogRef.current.showModal()
+    }
+
+    const closeModal = () => {
+        dialogRef.current.close()
+    }
+
+    return (
+        <div id="container">
+            <aside id="question-types">
+                <h2>Question Types</h2>
+
+                {allQuestionTypes.map((value, index) => {
+
+                    return <QuestionTypeButton key={index} title={value.name} typeValue={value.type} openFunc={openModal} />
+                })}
+            </aside>
+
+            <main id="question-container">
+                <Survey model={survey} />
+            </main>
+
+            <aside id="question-details">
+                <h2>Details</h2>
+                {/* TODO: Display form so users can set question details before adding to survey */}
+            </aside>
+
+            <dialog className='add-question-dialog' ref={dialogRef}>
+                <form className='add-question-form' onSubmit={addNewQuestion}>
+                    <label htmlFor="title-input">Question Title</label>
+                    <input type="text" ref={inputRef} id="title-input" placeholder='Enter question...' required />
+                    <div className='dialog-btns'>
+                        <button type='submit'>Create</button>
+                        <button onClick={closeModal}>Cancel</button>
+                    </div>
+                </form>
+            </dialog>
+        </div>
+    )
+}
+
+function QuestionTypeButton({ title, typeValue, openFunc }) {
+
     return <>
         <div className='question-type'>
             {title}
-            <button className='add-question-btn' onClick={e => dialogRef.current.showModal()}>Add</button>
+            <button className='add-question-btn' onClick={e => openFunc(typeValue)}>Select</button>
         </div>
-
-        <dialog className='add-question-dialog' ref={dialogRef}>
-            <form className='add-question-form' onSubmit={addNewQuestion}>
-                <label htmlFor="title-input">Question Title</label>
-                <input type="text" ref={inputRef} id="title-input" placeholder='Enter question...' />
-                <div className='dialog-btns'>
-                    <button type='submit'>Create</button>
-                    <button onClick={e => dialogRef.current.close()}>Cancel</button>
-                </div>
-            </form>
-        </dialog>
     </>
 }
