3e055148175ff847d06b3c130b11e575cd6ebade
Tarun Jeevan <tjeevan200@gmail.com>
2023-12-02 21:09:23 -0500

    Pretty much finished survey creator. Just need to connect to backend. Still need to implement question deletion though.

--- FrontEnd/src/Components/SurveyBuilder/SurveyBuilder.jsx
+++ FrontEnd/src/Components/SurveyBuilder/SurveyBuilder.jsx
@@ -1,22 +1,24 @@
 import 'survey-core/defaultV2.min.css'
 import { Model } from 'survey-core'
 import { Survey } from 'survey-react-ui'
-import { useRef, useState } from "react"
+import { useState } from "react"
 import "./SurveyBuilder.css"
+import { QuestionForm } from './QuestionForm'
 
 const allQuestionTypes = [
     { name: 'Textbox', type: 'text' },
     { name: 'Comment', type: 'comment' },
-    { name: 'Radio Button Group', type: 'radiogroup' }, // TODO: Needs additional options
-    { name: 'Rating Scale', type: 'rating' }, // TODO: Needs additional options
-    { name: 'Checkboxes', type: 'checkbox' }, // TODO: Needs additional options
-    { name: 'Dropdown', type: 'dropdown' }, // TODO: Needs additional options
-    { name: 'Multi-Select Dropdown', type: 'tagbox' }, // TODO: Needs additional options
+    { name: 'Radio Button Group', type: 'radiogroup' },
+    { name: 'Rating Scale', type: 'rating' },
+    { name: 'Checkboxes', type: 'checkbox' },
+    { name: 'Dropdown', type: 'dropdown' },
+    { name: 'Multi-Select Dropdown', type: 'tagbox' },
     { name: 'Yes/No', type: 'boolean' },
-    { name: 'Ranking', type: 'ranking' }, // TODO: Needs additional options
+    { name: 'Ranking', type: 'ranking' },
 ]
 
 export function SurveyBuilder() {
+    const [showDetails, setShowDetails] = useState(false)
     const [questionType, setQuestionType] = useState("")
     const [surveyJson, setSurveyJson] = useState({
         pages: [{
@@ -24,54 +26,25 @@ export function SurveyBuilder() {
             elements: []
         }]
     })
-    const dialogRef = useRef()
-    const inputRef = useRef()
 
     const survey = new Model(surveyJson)
 
-    const addNewQuestion = e => {
-        e.preventDefault()
-
-        const title = inputRef.current.value
-
-        setSurveyJson((prevJson) => {
-            return {
-                pages: [{
-                    name: "Page1",
-                    elements: [
-                        ...prevJson.pages[0].elements,
-                        {
-                            type: questionType,
-                            name: title.replace(/\s/g, ''),
-                            title: title
-                        }
-                    ]
-                }]
-            }
-        })
-
-        inputRef.current.value = ''
-        dialogRef.current.close()
-    }
-
-    const openModal = type => {
-        inputRef.current.value = ''
+    const openDetailsForm = type => {
         setQuestionType(type)
-        dialogRef.current.showModal()
-    }
-
-    const closeModal = () => {
-        dialogRef.current.close()
+        setShowDetails(true) // TODO: Put this in useEffect if it's not updating properly
     }
 
     return (
         <div id="container">
             <aside id="question-types">
                 <h2>Question Types</h2>
-
                 {allQuestionTypes.map((value, index) => {
-
-                    return <QuestionTypeButton key={index} title={value.name} typeValue={value.type} openFunc={openModal} />
+                    return (
+                        <div key={index} className='question-type'>
+                            {value.name}
+                            <button className='add-question-btn' onClick={e => openDetailsForm(value.type)}>Select</button>
+                        </div>
+                    )
                 })}
             </aside>
 
@@ -81,29 +54,10 @@ export function SurveyBuilder() {
 
             <aside id="question-details">
                 <h2>Details</h2>
-                {/* TODO: Display form so users can set question details before adding to survey */}
+                {showDetails &&
+                    <QuestionForm typeValue={questionType} setSurveyJson={setSurveyJson} setShowDetails={setShowDetails} />
+                }
             </aside>
-
-            <dialog className='add-question-dialog' ref={dialogRef}>
-                <form className='add-question-form' onSubmit={addNewQuestion}>
-                    <label htmlFor="title-input">Question Title</label>
-                    <input type="text" ref={inputRef} id="title-input" placeholder='Enter question...' required />
-                    <div className='dialog-btns'>
-                        <button type='submit'>Create</button>
-                        <button onClick={closeModal}>Cancel</button>
-                    </div>
-                </form>
-            </dialog>
-        </div>
-    )
-}
-
-function QuestionTypeButton({ title, typeValue, openFunc }) {
-
-    return (
-        <div className='question-type'>
-            {title}
-            <button className='add-question-btn' onClick={e => openFunc(typeValue)}>Select</button>
         </div>
     )
 }
