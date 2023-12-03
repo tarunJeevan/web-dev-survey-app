33f0d3767c7ed36731872451f41408225e825004
saipavanchowdari <epavanchowdary11@gmail.com>
2023-12-03 16:25:30 -0500

    username change and loginUI change

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -2,13 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import { Login } from './Components/Login/Login';
 import { Header } from './Components/Header/Header';
+import { createContext, useState } from 'react';
+import { Profile } from './Components/Profile/Profile';
+import { Dashboard } from './Components/Dashboard/Dashboard'
+import {SurveyBuilder} from './Components/SurveyBuilder/SurveyBuilder'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export const UserContext = createContext(null);
 
 function App() {
+  const[username, setusername] = useState(null);
   return (
-    <div className="App">
-      <Header />
-      <Login />
-    </div>
+    <UserContext.Provider value={{username, setusername}}>
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/creator" element={<SurveyBuilder />} />
+      </Routes>
+    </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,27 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App';
-import { Header } from './Components/Header/Header';
 import reportWebVitals from './reportWebVitals';
-import { Login } from './Components/Login/Login';
-import { Profile } from './Components/Profile/Profile';
-import { Dashboard } from './Components/Dashboard/Dashboard'
-import {SurveyBuilder} from './Components/SurveyBuilder/SurveyBuilder'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/creator" element={<SurveyBuilder />} />
-      </Routes>
-    </BrowserRouter>
+    <App/>   
   </React.StrictMode>
 );
 
