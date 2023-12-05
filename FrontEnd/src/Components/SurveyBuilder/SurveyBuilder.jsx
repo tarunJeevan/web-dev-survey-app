0993541d37c8d33fff1cd0324aae6f91e1f439bb
Tarun Jeevan <tjeevan200@gmail.com>
2023-12-05 11:31:13 -0500

    Added the saving functionality so survey is saved in backend

--- FrontEnd/src/Components/SurveyBuilder/SurveyBuilder.jsx
+++ FrontEnd/src/Components/SurveyBuilder/SurveyBuilder.jsx
@@ -1,6 +1,5 @@
 import 'survey-core/defaultV2.min.css'
 import 'survey-creator-core/survey-creator-core.min.css'
-import './SurveyBuilder.css'
 import { SurveyCreator, SurveyCreatorComponent } from 'survey-creator-react'
 
 // Options to determine what things users can see on the survey editor
@@ -11,7 +10,7 @@ const creatorOptions = {
 }
 
 export function SurveyBuilder() {
-// Survey creator object that contains the data that <SurveyCreatorComponent /> uses to render the editor
+    // Survey creator object that contains the data that <SurveyCreatorComponent /> uses to render the editor
     const creator = new SurveyCreator(creatorOptions)
 
     // The survey's native saving function. Here's the documentation if you want to get a better idea of how it works.
@@ -20,12 +19,12 @@ export function SurveyBuilder() {
         console.log(creator.JSON) // Just to see what a survey json object looks like
 
         // Sends the survey's json schema to the specified url
-        // saveSurveyJson(
-        //     'https://websurvey.biskilog.com/api/Survey/new',
-        //     creator.JSON,
-        //     saveNo,
-        //     callback
-        // )
+        saveSurveyJson(
+            'https://websurvey.biskilog.com/api/Survey/new',
+            creator.JSON,
+            saveNo,
+            callback
+        )
     }
 
     return (
@@ -36,19 +35,42 @@ export function SurveyBuilder() {
 // Function to handle sending the data to the backend
 async function saveSurveyJson(url, json, saveNo, callback) {
     const bearer = `Bearer ${localStorage.getItem('token')}`
+
+    const today = new Date().toISOString()
+    const researcher = localStorage.getItem('username')
+    const pages = JSON.stringify(json.pages)
+    const title = json.title
+    const description = json.description
+    const logoPosition = json.logoPosition
+    const logo = json.logo
+
+    const responseBody = {
+        id: 0,
+        title: title,
+        description: description,
+        logoPosition: logoPosition,
+        pages: pages,
+        dateCreated: today,
+        logo: logo,
+        researcher: researcher
+    }
     try {
         const response = await fetch(url,
             {
                 method: 'POST',
                 headers: { 'Authorization': bearer, 'Content-Type': 'application/json' },
-                body: JSON.stringify(json)
+                body: JSON.stringify(responseBody)
             }
         )
 
-        if (response.status === 200)
+        if (response.status === 200) {
+            console.log('saved successfully')
             callback(saveNo, true)
-        else
+        }
+        else {
+            console.log('saving failed')
             callback(saveNo, false)
+        }
     } catch (err) {
         callback(saveNo, false)
     }
