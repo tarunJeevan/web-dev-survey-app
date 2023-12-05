2f13953016cc587ccb078b60c519db512bdbeb36
saipavanchowdari <epavanchowdary11@gmail.com>
2023-12-04 20:31:14 -0500

    removed restrict route and removed comment

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -28,7 +28,6 @@ function App() {
   useEffect(()=>{
     const auth = getAuth();
     onAuthStateChanged(auth, (user) => {
-    console.log("Changed");
     if (user) {
     setloginpage(true);
     } 
