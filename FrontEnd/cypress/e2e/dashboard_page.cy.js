4cfcbf645aa07396d823e910b4d2b11429603cf2
Tarun Jeevan <tjeevan200@gmail.com>
2023-12-10 17:58:12 -0500

    short update on e2e
    Still need to fix login problem though

--- FrontEnd/cypress/e2e/dashboard_page.cy.js
+++ FrontEnd/cypress/e2e/dashboard_page.cy.js
@@ -2,23 +2,49 @@
 
 describe('Initial UX tests', () => {
     // it('sucessfully logs in', () => {
-    //     cy.visit('http://localhost:3000')
+    //     cy.visit('https://pfwsurvey.biskilog.com/')
 
     //     // Click on login button
     //     cy.get('LOGIN').click()
     // })
 
-    it('checks the dashboard', () => {
+    beforeEach(() => {
         cy.login()
-        // cy.visit('http://localhost:3000/dashboard')
+    })
 
-        // // Check if Tarun Jeevan is logged in
-        // cy.getAllLocalStorage()
+    it('checks the dashboard', () => {
+        cy.visit('https://pfwsurvey.biskilog.com/dashboard')
 
-        // // Does the Add button appear?
-        // cy.contains('Add')
+        // Check if Tarun Jeevan is logged in
+        cy.getAllLocalStorage()
 
-        // // Is .surveylist-container empty?
-        // cy.get('.surveylist-container').children().should('not.have.length', 0)
+        // Does the Add button appear?
+        cy.contains('Add')
+
+        // Is .surveylist-container empty?
+        cy.get('.surveylist-container').children().should('not.have.length', 0)
+    })
+
+    it('checks the navigation to survey creator', () => {
+        cy.visit('https://pfwsurvey.biskilog.com/dashboard')
+        cy.get('.add-btn').click()
+
+        // Check if this took the page to the right url
+        cy.location('pathname').should('eq', '/creator')
+    })
+
+    it('check the survey preview', () => {
+        cy.visit('https://pfwsurvey.biskilog.com/dashboard')
+        cy.get('.open-btn').first().click()
+
+        // Check if this took the page to the right url
+        cy.location('pathname').should('eq', '/survey')
+    })
+
+    it('checks logout functionality', () => {
+        cy.visit('https://pfwsurvey.biskilog.com/dashboard')
+
+        // Try logout
+        cy.get('#basic-navbar-nav') // TODO: Continue
     })
 })
